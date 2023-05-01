7af6003a5302f987731f77f9ce7e36a5730699cc
boo Bae <chidiekwebelam@gmail.com>
2023-05-01 15:38:00 +0100

    created new components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -16,7 +16,7 @@ const Navigation = () => {
               <Link className='nav-link' to='/shop'>
                SHOP 
               </Link>
-              <Link className='nav-link' to='/sign-in'>
+              <Link className='nav-linka' to='/sign-in'>
               SIGN IN
               </Link>
           </div>
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,21 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { 
+         signInWithGooglePopup,
+         createUserDocumentFromAuth, 
+        } from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
-    const logGooleUser = async () => {
-        const {user} = await signInWithGooglePopup();
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     };
 
     return (
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGooleUser}>Sign in with Google Popup</button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <SignUpForm />
         </div>
     );
 };
