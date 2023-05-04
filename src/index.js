f15e6db0f8ceb545164a22a12239da40f9f66a14
boo Bae <chidiekwebelam@gmail.com>
2023-05-04 13:19:33 +0100

    adding cart context as well as shopping function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
+
 import './index.scss';
 
 import reportWebVitals from './reportWebVitals';
@@ -12,7 +15,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,21 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
-import { ReactComponent as BluefishLogo } from '../../assets/bluefishfarm.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
+import { ReactComponent as BluefishLogo } from '../../assets/bluefishfarm.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-
+    const { isCartOpen } = useContext(CartContext);
 
     return (
       <Fragment>
@@ -33,7 +37,9 @@ const Navigation = () => {
               SIGN IN
               </Link>
               )} 
+              <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
