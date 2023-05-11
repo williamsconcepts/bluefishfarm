aea6c2607541a7ab72f7f9c4b9e4c2f1ef7f8eb6
boo Bae <chidiekwebelam@gmail.com>
2023-05-11 13:22:23 +0100

    ready to deploy

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,31 +6,31 @@ const categories = [
     {
       id: 1,
       title: 'Fingerlings',
-      imageUrl: 'https://drive.google.com/file/d/1Ada1jf5fG8NH81AeKnNRa57ftbeEZDD3/view?usp=share_link',
+      imageUrl: 'https://greentrrb.com/bluefish/Hybrid-Heterobranchius-Clarias-gariepinus-Custom.jpeg',
       route: 'shop/fingerlings'
     },
     {
       id: 2,
       title: 'Juveniles',
-      imageUrl: 'https://drive.google.com/file/d/14JouX9s6LD-1mMbdcrc5kIQaPR_DFjOX/view?usp=share_link',
+      imageUrl: 'https://greentrrb.com/bluefish/juveniles.jpeg',
       route: 'shop/juveniles'
     },
     {
       id: 3,
       title: 'BBQ Sizes',
-      imageUrl: 'https://drive.google.com/file/d/1smvBpsbSh1kAzByxpCfCqKdqWiYHTGWd/view?usp=share_link',
+      imageUrl: 'https://greentrrb.com/bluefish/African-catfish-broodstock-2-Custom.jpeg',
       route: 'shop/bbq sizes'
     },
     {
       id: 4,
       title: 'Smoked/Smokables',
-      imageUrl: 'https://drive.google.com/file/d/12Ag1H3ZDzTc41-iI1ke5Qmc265NOUh2H/view?usp=share_link',
+      imageUrl: 'https://greentrrb.com/bluefish/shopping.png',
       route: 'shop/smoked-fish'
     },
     {
       id: 5,
       title: 'Poultry Birds',
-      imageUrl: 'https://drive.google.com/file/d/1-gHyJ49Wr35qV0ZXC0YM3Zbgts4rmBMZ/view?usp=share_link',
+      imageUrl: 'https://greentrrb.com/bluefish/poultry.jpeg',
       route: 'shop/poultry birds'
     },
   ];
